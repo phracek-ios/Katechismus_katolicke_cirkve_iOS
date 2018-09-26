03a5daceb93c843b63dcaf49f257095f97e7dffb
Petr "Stone" Hracek <phracek@redhat.com>
2018-09-26 22:20:38 +0200

    Use WKWebView in About project
    Signed-off-by: Petr "Stone" Hracek <phracek@redhat.com>

--- KatechismusKatolickeCirkve/KatechismusKatolickeCirkve/Controllers/AboutProjectViewController.swift
+++ KatechismusKatolickeCirkve/KatechismusKatolickeCirkve/Controllers/AboutProjectViewController.swift
@@ -7,11 +7,12 @@
 //
 
 import UIKit
+import WebKit
 
 class AboutProjectViewController: UIViewController, UITextViewDelegate{
 
     fileprivate var catechismStructure: CatechismStructure?
-    @IBOutlet weak var aboutPrjTextView: UITextView!
+    @IBOutlet weak var aboutPrjWebView: WKWebView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,45 +20,9 @@ class AboutProjectViewController: UIViewController, UITextViewDelegate{
         catechismStructure = CatechismDataService.shared.catechismStructure
         // Do any additional setup after loading the view.
         guard let catechismStructure = catechismStructure else { return }
+        let htmlString = catechismStructure.about_project_1 + "<a href=\"https://www.ikarmel.cz\">Karmelitánské nakladatelství</a>" + catechismStructure.about_project_1a + "<a href=\"http://www.donum.cz\">Donum</a> nebo u <a href=\"https://www.paulinky.cz\">Paulínek</a>.<br><br>" + catechismStructure.about_project_1b + "<a href=\"https://www.cirkev.cz\">České biskupské konference</a>" + catechismStructure.about_project_1c + catechismStructure.about_project_2 + catechismStructure.about_project_3 + "<a href=\"https://www.ikarmel.cz\">Karmelitánské nakladatelství</a>" + catechismStructure.about_project_3a
+        aboutPrjWebView.loadHTMLString("<html><body><font size=18" + htmlString + "</body></html>", baseURL: nil)
         
-        aboutPrjTextView.dataDetectorTypes = .link
-        aboutPrjTextView.isSelectable = true
-
-        let attributedPrjFirst = NSMutableAttributedString(string: catechismStructure.about_project_1)
-        let carmelPublishingMutAttrString = NSMutableAttributedString(string: "Karmelitánské nakladatelství")
-        let paulinPublishingMutAttrString = NSMutableAttributedString(string: "Paulínek")
-        let donumPublishingMutAttrString = NSMutableAttributedString(string: "Donum")
-        let cbkPublishingMutAttrString = NSMutableAttributedString(string: "České biskupstké konference")
-        let carmelURL: String = "https://www.ikarmel.cz"
-        let paulinURL: String = "https://www.paulinky.cz"
-        let donumURL: String = "http://www.donum.cz"
-        let cbkURL: String = "https://www.cirkev.cz"
-        
-        carmelPublishingMutAttrString.addAttribute(.link, value: carmelURL, range: NSRange(location: 0, length: carmelPublishingMutAttrString.length))
-        attributedPrjFirst.append(carmelPublishingMutAttrString)
-        attributedPrjFirst.append(NSAttributedString(string: catechismStructure.about_project_1a))
-
-        donumPublishingMutAttrString.addAttribute(.link, value: donumURL, range: NSRange(location: 0, length: donumPublishingMutAttrString.length))
-        attributedPrjFirst.append(donumPublishingMutAttrString)
-        attributedPrjFirst.append(NSAttributedString(string: " nebo u "))
-
-        paulinPublishingMutAttrString.addAttribute(.link, value: paulinURL, range: NSRange(location: 0, length: paulinPublishingMutAttrString.length))
-        attributedPrjFirst.append(paulinPublishingMutAttrString)
-        attributedPrjFirst.append(NSAttributedString(string: ".\n\n"))
- 
-        attributedPrjFirst.append(NSAttributedString(string: catechismStructure.about_project_1b))
-
-        cbkPublishingMutAttrString.addAttribute(.link, value: cbkURL, range: NSRange(location: 0, length: cbkPublishingMutAttrString.length))
-        attributedPrjFirst.append(cbkPublishingMutAttrString)
-        attributedPrjFirst.append(NSAttributedString(string: catechismStructure.about_project_1c))
-
-        attributedPrjFirst.append(NSAttributedString(string: catechismStructure.about_project_2))
-
-        attributedPrjFirst.append(NSAttributedString(string: catechismStructure.about_project_3))
-        attributedPrjFirst.append(carmelPublishingMutAttrString)
-        attributedPrjFirst.append(NSAttributedString(string: catechismStructure.about_project_3a))
-       
-        aboutPrjTextView.attributedText = attributedPrjFirst
     }
 
     override func didReceiveMemoryWarning() {
