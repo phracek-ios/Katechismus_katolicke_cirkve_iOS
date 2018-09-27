7b8098dc8ebfb689af0fda9ab203bf5f9d5d2e0b
Petr "Stone" Hracek <phracek@redhat.com>
2018-09-27 17:10:41 +0200

    Use WKWebView instead of Attributed String in About Storyboard
    Signed-off-by: Petr "Stone" Hracek <phracek@redhat.com>

--- KatechismusKatolickeCirkve/KatechismusKatolickeCirkve/Controllers/AboutViewController.swift
+++ KatechismusKatolickeCirkve/KatechismusKatolickeCirkve/Controllers/AboutViewController.swift
@@ -8,54 +8,32 @@
 
 import UIKit
 import Foundation
-
+import WebKit
 
 class AboutViewController: UIViewController, UITextViewDelegate {
 
     //MARK: Properties
     fileprivate var catechismStructure: CatechismStructure?
-    @IBOutlet weak var aboutMainTextView: UITextView!
-    
+    @IBOutlet weak var aboutMainWebView: WKWebView!
     override func viewDidLoad() {
         super.viewDidLoad()
         catechismStructure = CatechismDataService.shared.catechismStructure
         // Do any additional setup after loading the view.
         guard let catechismStructure = catechismStructure else { return }
-        aboutMainTextView.dataDetectorTypes = .link
-        aboutMainTextView.isEditable = false
-        aboutMainTextView.isSelectable = false
-
-        let attributedNewLine = NSMutableAttributedString(string: "\n")
-        let attributedAboutFirst = NSMutableAttributedString(string: catechismStructure.about_appl_1)
         
-        let delphsysMutAttrString = NSMutableAttributedString(string: "DelpSys")
-        let delpsysURL: String = "https://www.delpsys.cz"
-        delphsysMutAttrString.addAttribute(.link, value: delpsysURL, range: NSRange(location: 0, length: delphsysMutAttrString.length))
-        attributedAboutFirst.append(delphsysMutAttrString)
-
-        attributedAboutFirst.append(attributedNewLine)
-        let delpsys = UIImage(named: "delpsys")
-        let imageAttachment = NSTextAttachment()
-        let newImageWidth = (aboutMainTextView.bounds.size.width - 10)
-        let scale = newImageWidth / (delpsys?.size.width)!
-        let newImageHeight = (delpsys?.size.height)! * scale
-        imageAttachment.bounds = CGRect.init(x: 0, y: 0, width: newImageWidth, height: newImageHeight)
-
-        imageAttachment.image = delpsys
-
-        let attrStringWithImage = NSAttributedString(attachment: imageAttachment)
-        attributedAboutFirst.append(attrStringWithImage)
-        attributedAboutFirst.append(NSAttributedString(string: catechismStructure.about_appl_2))
-        let email: String = "<a href=\"phracek@gmail.com\">phracek@gmail.com</a>"
-        attributedAboutFirst.append(email.htmlToAttributedString!)
-        attributedAboutFirst.append(NSAttributedString(string: catechismStructure.about_appl_2a))
-
-        let issuesMutAttrString = NSMutableAttributedString(string: "zde.")
-        let issuesURL: String = "https://github.com/phracek/Katechismus_katolicke_cirkve_iOS/issues"
-        issuesMutAttrString.addAttribute(.link, value: issuesURL, range: NSRange(location: 0, length: issuesMutAttrString.length))
-        attributedAboutFirst.append(issuesMutAttrString)
-
-        aboutMainTextView.attributedText = attributedAboutFirst
+        let htmlStringPart1 = catechismStructure.about_appl_1 + "\n"
+        var htmlImage: String = ""
+        if let image = UIImage(named: "delpsys"),
+            let data = UIImagePNGRepresentation(image) {
+                let base64 = data.base64EncodedString(options: [])
+                let url = "data:application/png;base64," + base64
+            htmlImage = "<img style=\"width: 100%\" src='\(url)'>"
+        }
+        
+        let htmlStringPart2 = catechismStructure.about_appl_2 +
+            "<a href=\"phracek@gmail.com\">phracek@gmail.com</a>" +
+            catechismStructure.about_appl_2a + "<a href=\"https://github.com/phracek/Katechismus_katolicke_cirkve_iOS/issues\">zde.</a>"
+        aboutMainWebView.loadHTMLString("<html><body><font size=18" + htmlStringPart1 + "<br><br>" + htmlImage + htmlStringPart2 + "</body></html>", baseURL: nil)
     }
 
     override func didReceiveMemoryWarning() {
